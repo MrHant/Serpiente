8fde99131681e162b8ade71d1e2893de90df8658
unknown <root@.(none)>
2010-07-19 20:10:19 +0300

    initial commit

---
+++ src/serpiente/Main.hx
@@ -0,0 +1,84 @@
+﻿package serpiente;
+
+import flash.display.MovieClip;
+import flash.events.Event;
+import flash.events.MouseEvent;
+import flash.Lib;
+
+import serpiente.SButton;
+
+/**
+ * ...
+ * @author MrHant
+ */
+
+class Main 
+{
+	public static var wrapper: Dynamic;
+	public static var mc : MovieClip = flash.Lib.current;
+
+	
+	
+	static function main() 
+	{
+		var WRAP = new Wrap();
+
+
+
+/*      	but3.
+ /*
+		addEventListener(MouseEvent.CLICK, function(e: MouseEvent) {
+      		wrapper.external.showInviteBox();
+      	});
+      	but4.addEventListener(MouseEvent.CLICK, function(e: MouseEvent) {
+      		wrapper.external.showInstallBox();
+      	});		
+*/	
+
+	}
+	
+
+}
+
+class Wrap {
+	public static var but1 : serpiente.SButton;
+	public static var but2 : serpiente.SButton;
+	public static var but3 : serpiente.SButton;
+	public static var but4 : serpiente.SButton;
+	
+	public function new() {
+#if debug
+		onAddedToStage();
+#else
+		Main.mc.stage.addEventListener(Event.ADDED_TO_STAGE, onAddedToStage); 
+#end
+	}
+	
+	public function onAddedToStage(?e:Event)
+	{
+    	Main.wrapper = Main.mc.parent.parent;
+		
+		
+		var startPageBG = Lib.attach("firstScreen");
+		startPageBG.x = -1;
+		startPageBG.y = -1;
+#if debug
+		Lib.current.stage.addChild(startPageBG);
+#else
+		Main.wrapper.addChild(startPageBG);
+#end
+		but1 = new serpiente.SButton(200, 100, "Начать игру");
+		but2 = new serpiente.SButton(200, 150, "Справка");
+		but3 = new serpiente.SButton(200, 200, "Пригласить друзей",25);
+		but4 = new serpiente.SButton(200, 250, "Добавить на страницу",25);
+		//Main.but
+		but3.but.addEventListener(MouseEvent.CLICK, function(e: MouseEvent) {
+      		Main.wrapper.external.showInviteBox();
+      	});
+		but4.but.addEventListener(MouseEvent.CLICK, function(e: MouseEvent) {
+      		Main.wrapper.external.showInstallBox();
+      	});
+	}
+     
+
+}
---
+++ src/serpiente/SButton.hx
@@ -0,0 +1,42 @@
+﻿/**
+ * ...
+ * @author MrHant
+ */
+
+
+package serpiente;
+
+
+import flash.display.SimpleButton;
+import flash.Lib;
+import flash.text.TextField;
+import flash.text.TextFieldAutoSize;
+import flash.text.TextFormat;
+
+class SButton 
+{
+    public var but : SimpleButton ;
+
+	public function new(x: Int, y:Int, str:String,size:Int=40) 
+	{
+		but = new SimpleButton(Lib.attach("SButtonBG"));
+		but.overState = Lib.attach("SButtonAnimation");
+		but.hitTestState = Lib.attach("SButtonPlaceholder");
+		but.useHandCursor = true;
+		but.x = x;
+		but.y = y;
+		
+		var txt : TextField = new TextField();
+		txt.defaultTextFormat = new TextFormat("Monotype Corsiva",size,5983628);
+		txt.text = str;
+		txt.x = x + 150;
+		txt.y = y+40-size;
+		txt.width = 300;
+		txt.selectable = false;
+		
+		Main.mc.stage.addChild(txt);
+		
+		Main.mc.stage.addChild(but);
+	}
+	
+}
