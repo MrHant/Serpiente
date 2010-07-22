3021e3862e288f7a15d4f9011ec186d3ad39a254
Artsiomi Silivonchyk <mr.hant@gmail.com>
2010-07-22 21:06:00 +0300

    counter added

--- src/serpiente/Game.hx
+++ src/serpiente/Game.hx
@@ -7,20 +7,35 @@ package serpiente;
 
 import flash.display.MovieClip;
 import flash.Lib;
+import flash.events.Event;
 
 class Game 
 {
-
+	var counter: MovieClip;
 	public function new() 
 	{
-		var counter: MovieClip = Lib.attach("counter");
+		counter = Lib.attach("counter");
 		counter.x = 300;
 		counter.y = 275;
-#if debug
+#if debug	
 		Main.mc.stage.addChild(counter);
 #else
 		Main.wrapper.addChild(counter);
 #end	
+		
+		counter.addEventListener(Event.ENTER_FRAME, onEnterFrame);
 	}
 	
+	
+	function onEnterFrame(evt:Event) {
+		if (evt.target.currentFrame == evt.target.totalFrames) {
+#if debug
+			Main.mc.stage.removeChild(counter);
+#else
+			Main.wrapper.removeChild(counter);
+#end				
+			counter.stop();
+			counter.removeEventListener(Event.ENTER_FRAME, onEnterFrame);
+		}
+	}
 }
